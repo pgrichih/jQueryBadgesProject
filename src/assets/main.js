28bb522725e7afccc90856d6ad6a2716ceab0f39
Richard Holzner <richard@polargold.de>
2017-06-06 16:32:04 +0200

    Add jquery ajax function

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,56 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+    // your code will go here
+
+    coursesCompleted = [];
+
+    console.log(coursesCompleted);
+
+    $.ajax({
+        url     : 'https://www.codeschool.com/users/richih.json',
+        dataType: 'jsonp',
+        success : function (response) {
+
+            $('#badges').html(response);
+            console.log(response.courses.completed);
+            addCourses(response.courses.completed);
+
+        }
+    });
+
+    function addCourses(courses) {
+
+        var badges = $('#badges');
+
+        courses.forEach(function (course) {
+            console.log(course.title);
+            console.log(course.badge);
+
+            var $course = $('<div>', {
+                'class': 'course'
+            }).appendTo(badges);
+
+            $('<h3>', {
+                text: course.title
+            }).appendTo($course);
+
+            $('<img>', {
+                src: course.badge
+            }).appendTo($course);
+
+            $('<a>', {
+                href: course.url,
+                text: 'See Course',
+                target: 'blank',
+                class: 'btn btn-primary'
+            }).appendTo($course);
+
+        });
+
+
+    }
+
+
+    // https://www.codeschool.com/users/richih.json
 
 });
